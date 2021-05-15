68b43980e060e0f56e4fcc80c10202b6422ab284
Sumit Verma <sumitverma.oist@gmail.com>
2021-05-15 19:55:53 +0530

    Added match page in react

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,9 +5,29 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+    const [matches,setMatches] = useState([]);
+    const {teamName,year} = useParams();
+
+    //const teamName = 'Delhi Capitals';
+    useEffect(
+        () => {
+        
+        const fetchMatches = async () => {
+          
+          const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+          const data = await response.json();
+          setMatches(data);
+          console.log(matches);
+    
+        };
+    
+        fetchMatches();
+      },[]);
+
   return (
     <div className="MatchPage">
         <h1>Match Page</h1>
+        {matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)}
     </div>
   );
 }
